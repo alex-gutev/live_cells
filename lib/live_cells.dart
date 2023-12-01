d762b9ab4ac8889d9267dc3ec52c922607f7c9a8
Alexander Gutev <alex.gutev@gmail.com>
2023-12-01 19:00:25 +0100

    Add equality comparison cells.

--- lib/live_cells.dart
+++ lib/live_cells.dart
@@ -1,3 +1,5 @@
 library live_cells;
 
 export 'src/value_cell.dart';
+export 'src/equality/eq_cell.dart';
+export 'src/equality/neq_cell.dart';
---
+++ lib/src/equality/neq_cell.dart
@@ -0,0 +1,41 @@
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
+
+import '../value_cell.dart';
+import 'eq_cell.dart';
+
+/// A cell which evaluates to true when the values of two cells are not equal under `==`.
+///
+/// [T] is the value type of the first cell, and [U] is the value type of the second cell.
+class NeqCell<T,U> extends ValueCell<bool> {
+  NeqCell(this._cellA, this._cellB) :
+        _listenable = Listenable.merge([_cellA, _cellB]);
+
+  @override
+  bool get value => _cellA.value != _cellB.value;
+
+  @override
+  void addListener(VoidCallback listener) {
+    _listenable.addListener(listener);
+  }
+
+  @override
+  void removeListener(VoidCallback listener) {
+    _listenable.removeListener(listener);
+  }
+
+  @override
+  ValueCell<bool> eq<P>(ValueCell<P> other) => EqCell(this, other);
+
+  @override
+  ValueCell<bool> neq<P>(ValueCell<P> other) => NeqCell(this, other);
+
+  /// Private
+
+  final ValueListenable<T> _cellA;
+  final ValueListenable<U> _cellB;
+
+  /// The [Listenable] to which observers are added
+  final Listenable _listenable;
+}
