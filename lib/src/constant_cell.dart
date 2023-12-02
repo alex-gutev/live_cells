b1c2b179814a70bea726b04247421b1ac99f0342
Alexander Gutev <alex.gutev@gmail.com>
2023-12-02 09:08:43 +0100

    Add constant cell.

---
+++ lib/src/constant_cell.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+import 'equality/cell_equality.dart';
+import 'value_cell.dart';
+
+/// A cell with a constant value.
+///
+/// This class implements the [ValueCell] interface but its listeners
+/// are never called since the cell's value does not change.
+class ConstantCell<T> extends ValueCell<T> with CellEquality<T> {
+  ConstantCell(this._value);
+
+  @override
+  T get value => _value;
+
+  @override
+  void addListener(VoidCallback listener) {
+    // Do nothing since the value of the cell never changes
+  }
+
+  @override
+  void removeListener(VoidCallback listener) {
+    // Do nothing since the value of the cell never changes
+  }
+
+  /// Private
+
+  final T _value;
+}
