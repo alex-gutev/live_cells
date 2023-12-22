81830db63a71fd8bc4310f4ccdc58128f23e5cec
Alexander Gutev <alex.gutev@gmail.com>
2023-12-22 09:19:21 +0100

    Switch factorial example to CellBuilder.

--- example/lib/demos/store_cell_demo.dart
+++ example/lib/demos/store_cell_demo.dart
@@ -8,58 +8,60 @@ class StoreCellDemo extends StatefulWidget {
 
 class _StoreCellDemoState extends State<StoreCellDemo> {
   final n = MutableCell(0);
-  
-  late final factorial = n.apply((n) {
-    var result = 1;
 
-    // This should only be printed once per change of value of n
-    print('Computing Factorial of $n');
-    
-    for (var i = 2; i <= n; i++) {
-      result *= i;
-    }
-    
-    return result;
-  }).store();
-  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Store Cells'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Demonstration of cells which store the value of cell in memory',
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                    fontWeight: FontWeight.bold
+    return CellBuilder(
+      create: () => n.apply((n) {
+        var result = 1;
+
+        // This should only be printed once per change of value of n
+        print('Computing Factorial of $n');
+
+        for (var i = 2; i <= n; i++) {
+          result *= i;
+        }
+
+        return result;
+      }).store(),
+
+      builder: (context, factorial, _) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Store Cells'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Demonstration of cells which store the value of cell in memory',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold
+                  ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                  children: [
-                    Text('N:'),
-                    const SizedBox(width: 5),
-                    Expanded(
-                      child: TextField(
-                        keyboardType: TextInputType.numberWithOptions(signed: true),
-                        onChanged: (value) {
-                          n.value = int.tryParse(value) ?? 0;
-                        },
-                      ),
-                    )
-                  ]
-              ),
-              const SizedBox(height: 10),
-              factorial.toWidget((context, fact, _) => Text('N! = $fact')),
-              factorial.toWidget((context, fact, child) => Text('The factorial of N is $fact'))
-            ],
+                const SizedBox(height: 10),
+                Row(
+                    children: [
+                      Text('N:'),
+                      const SizedBox(width: 5),
+                      Expanded(
+                        child: TextField(
+                          keyboardType: TextInputType.numberWithOptions(signed: true),
+                          onChanged: (value) {
+                            n.value = int.tryParse(value) ?? 0;
+                          },
+                        ),
+                      )
+                    ]
+                ),
+                const SizedBox(height: 10),
+                factorial.toWidget((context, fact, _) => Text('N! = $fact')),
+                factorial.toWidget((context, fact, child) => Text('The factorial of N is $fact'))
+              ],
+            ),
           ),
         ),
       ),
