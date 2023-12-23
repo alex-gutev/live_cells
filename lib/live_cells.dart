d9e78a9d20a451707c30f087d4871b5a79493aa1
Alexander Gutev <alex.gutev@gmail.com>
2023-12-23 08:53:15 +0100

    Add show list to library exports.

--- lib/live_cells.dart
+++ lib/live_cells.dart
@@ -1,13 +1,16 @@
 library live_cells;
 
-export 'src/value_cell.dart';
-export 'src/base/managed_cell.dart';
-export 'src/base/cell_listeners.dart';
-export 'src/base/notifier_cell.dart';
-export 'src/mutable_cell/store_cell.dart';
-export 'src/mutable_cell/mutable_cell.dart';
-export 'src/compute_cell/compute_cell.dart';
-export 'src/compute_cell/compute_extension.dart';
-export 'src/async_cell/delay_cell.dart';
-export 'src/widgets/widget_extension.dart';
-export 'src/widgets/cell_builder.dart';
+export 'src/value_cell.dart'
+    show ValueCell, ConstantCell, DependentCell, CellEquality, EqCell, NeqCell;
+
+export 'src/base/managed_cell.dart' show ManagedCell;
+export 'src/base/cell_listeners.dart' show CellListeners;
+export 'src/base/notifier_cell.dart' show NotifierCell;
+export 'src/mutable_cell/store_cell.dart' show StoreCell, StoreCellExtension;
+export 'src/mutable_cell/mutable_cell.dart' show MutableCell;
+export 'src/compute_cell/compute_cell.dart' show ComputeCell;
+export 'src/compute_cell/compute_extension.dart' show ComputeExtension;
+export 'src/async_cell/delay_cell.dart' show DelayCell;
+export 'src/widgets/widget_extension.dart' show WidgetExtension;
+export 'src/widgets/cell_builder.dart'
+    show CreateCell, BuildCellWidget, CellBuilder;
