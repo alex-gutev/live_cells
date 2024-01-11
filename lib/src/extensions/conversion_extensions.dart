8a68fe374b42950dedfeb7a3d768175aa7de2126
Alexander Gutev <alex.gutev@gmail.com>
2024-01-11 18:17:22 +0100

    Replace `error` parameter in `mutableString` with error handling via `Maybe`.

--- lib/src/extensions/conversion_extensions.dart
+++ lib/src/extensions/conversion_extensions.dart
@@ -1,5 +1,6 @@
 import '../value_cell.dart';
 import 'compute_extension.dart';
+import '../maybe_cell/maybe.dart';
 import '../mutable_cell/mutable_cell.dart';
 
 /// Provides methods for converting an integer to a string and vice versa.
@@ -10,42 +11,22 @@ extension ParseIntExtension on MutableCell<int> {
   /// cell is set explicitly, an integer is parsed from the assigned string
   /// value and is assigned to this cell's value.
   ///
-  /// If [error] is given, any exception thrown while parsing an integer
-  /// from the value assigned to the returned cell, is assigned to the cell
-  /// [error]. If there are no errors `null` is assigned.
-  ///
   /// [errorValue] is the value to assign to [this] cell when there is an
-  /// error parsing an integer. If this is `null`, no value is assigned to [this]
-  /// in the case of errors.
+  /// error parsing an integer from the string.
   ///
-  /// If [radix] is given the integer is parsed using the given radix.
-  ///
-  /// **Note** [radix] and [errorValue] are [ValueCell]'s to allow the radix
-  /// and default value to be chosen dynamically. If a constant is required use
-  /// the [NumValueCellExtension.cell] property e.g. `10.cell`.
+  /// **Note** [errorValue] is a [ValueCell]'s to allow the default value to be
+  /// chosen dynamically. If a constant is required use the
+  /// [NumValueCellExtension.cell] property e.g. `10.cell`.
   ///
   /// **Pitfalls**:
   /// 
-  /// A binding is not established between [errorValue], [radix] and [this] 
-  /// cell. This means that [this] cell will not react to changes in [errorValue]
-  /// or [radix]. Only the values at the time of assigning a value to the returned
-  /// cell are used.
+  /// A binding is not established between [errorValue] and [this] cell. This
+  /// means that [this] cell will not react to changes in [errorValue]. Only
+  /// the values at the time of assigning a value to the returned cell are used.
   MutableCell<String> mutableString({
-    MutableCell? error,
-    ValueCell<int>? errorValue = const ValueCell.value(0),
-    ValueCell<int>? radix,
+    ValueCell<int> errorValue = const ValueCell.value(0),
   }) => [this].mutableComputeCell(() => value.toString(), (value) {
-    try {
-      this.value = int.parse(value, radix: radix?.value);
-      error?.value = null;
-    }
-    catch (e) {
-      error?.value = e;
-
-      if (errorValue != null) {
-        this.value = errorValue.value;
-      }
-    }
+    this.value = int.tryParse(value) ?? errorValue.value;
   });
 }
 
@@ -54,16 +35,11 @@ extension ParseDoubleExtension on MutableCell<double> {
   /// Return a cell which evaluates to a string representation of this cell's value.
   ///
   /// The returned cell is a mutable computed cell. If the value of the returned
-  /// cell is set explicitly, an integer is parsed from the assigned string
+  /// cell is set explicitly, a double is parsed from the assigned string
   /// value and is assigned to this cell's value.
   ///
-  /// If [error] is given, any exception thrown while parsing an integer
-  /// from the value assigned to the returned cell, is assigned to the cell
-  /// [error]. If there are no errors `null` is assigned.
-  ///
   /// [errorValue] is the value to assign to [this] cell when there is an
-  /// error parsing an integer. If this is `null`, no value is assigned to [this]
-  /// in the case of errors.
+  /// error parsing a a double from the string.
   ///
   /// **Note** [errorValue] is a [ValueCell] to allow the default value
   /// to be chosen dynamically. If a constant is required use
@@ -73,23 +49,12 @@ extension ParseDoubleExtension on MutableCell<double> {
   ///
   /// A binding is not established between [errorValue] and [this] cell.
   /// This means that [this] cell will not react to changes in [errorValue].
-  /// Only the values at the time of assigning a value to the returned
+  /// Only the value at the time of assigning a value to the returned
   /// cell are used.
   MutableCell<String> mutableString({
-    MutableCell? error,
-    ValueCell<double>? errorValue = const ValueCell.value(0.0),
+    ValueCell<double> errorValue = const ValueCell.value(0.0),
   }) => [this].mutableComputeCell(() => value.toString(), (value) {
-    try {
-      this.value = double.parse(value);
-      error?.value = null;
-    }
-    catch (e) {
-      error?.value = e;
-
-      if (errorValue != null) {
-        this.value = errorValue.value;
-      }
-    }
+    this.value = double.tryParse(value) ?? errorValue.value;
   });
 }
 
@@ -101,13 +66,8 @@ extension ParseNumExtension on MutableCell<num> {
   /// cell is set explicitly, a [num] is parsed from the assigned string
   /// value and is assigned to this cell's value.
   ///
-  /// If [error] is given, any exception thrown while parsing an integer
-  /// from the value assigned to the returned cell, is assigned to the cell
-  /// [error]. If there are no errors `null` is assigned.
-  ///
   /// [errorValue] is the value to assign to [this] cell when there is an
-  /// error parsing an integer. If this is `null`, no value is assigned to [this]
-  /// in the case of errors.
+  /// error parsing a [num] from the string.
   ///
   /// **Note** [errorValue] is a [ValueCell] to allow the default value
   /// to be chosen dynamically. If a constant is required use
@@ -117,23 +77,12 @@ extension ParseNumExtension on MutableCell<num> {
   ///
   /// A binding is not established between [errorValue] and [this] cell.
   /// This means that [this] cell will not react to changes in [errorValue].
-  /// Only the values at the time of assigning a value to the returned
+  /// Only the value at the time of assigning a value to the returned
   /// cell are used.
   MutableCell<String> mutableString({
-    MutableCell? error,
-    ValueCell<num>? errorValue = const ValueCell.value(0),
+    ValueCell<num> errorValue = const ValueCell.value(0),
   }) => [this].mutableComputeCell(() => value.toString(), (value) {
-    try {
-      this.value = num.parse(value);
-      error?.value = null;
-    }
-    catch (e) {
-      error?.value = e;
-
-      if (errorValue != null) {
-        this.value = errorValue.value;
-      }
-    }
+    this.value = num.tryParse(value) ?? errorValue.value;
   });
 }
 
@@ -141,4 +90,52 @@ extension ParseNumExtension on MutableCell<num> {
 extension ConvertStringExtension on MutableCell<String> {
   /// Simply returns this cell
   MutableCell<String> mutableString() => this;
+}
+
+/// Provides methods for converting an integer to a string and vice versa with error handling.
+extension ParseMaybeIntExtension on MaybeCell<int> {
+  /// Return a cell which evaluates to a string representation of this cell's value.
+  ///
+  /// The returned cell is a mutable computed cell. If the value of the returned
+  /// cell is set explicitly, an integer is parsed from the assigned string
+  /// value, wrapped in a [Maybe], which is assigned to this cell's value.
+  ///
+  /// If an exception is thrown during parsing, a [Maybe] holding the exception
+  /// is assigned to this cell's value.
+  MutableCell<String> mutableString() =>
+      [this].mutableComputeCell(() => value.unwrap.toString(), (value) {
+        this.value = Maybe.wrap(() => int.parse(value));
+      });
+}
+
+/// Provides methods for converting a double to a string and vice versa with error handling.
+extension ParseMaybeDoubleExtension on MaybeCell<double> {
+  /// Return a cell which evaluates to a string representation of this cell's value.
+  ///
+  /// The returned cell is a mutable computed cell. If the value of the returned
+  /// cell is set explicitly, a double is parsed from the assigned string
+  /// value, wrapped in a [Maybe], which is assigned to this cell's value.
+  ///
+  /// If an exception is thrown during parsing, a [Maybe] holding the exception
+  /// is assigned to this cell's value.
+  MutableCell<String> mutableString() =>
+      [this].mutableComputeCell(() => value.unwrap.toString(), (value) {
+        this.value = Maybe.wrap(() => double.parse(value));
+      });
+}
+
+/// Provides methods for converting a [num] to a string and vice versa with error handling.
+extension ParseMaybeNumExtension on MaybeCell<num> {
+  /// Return a cell which evaluates to a string representation of this cell's value.
+  ///
+  /// The returned cell is a mutable computed cell. If the value of the returned
+  /// cell is set explicitly, a [num] is parsed from the assigned string
+  /// value, wrapped in a [Maybe], which is assigned to this cell's value.
+  ///
+  /// If an exception is thrown during parsing, a [Maybe] holding the exception
+  /// is assigned to this cell's value.
+  MutableCell<String> mutableString() =>
+      [this].mutableComputeCell(() => value.unwrap.toString(), (value) {
+        this.value = Maybe.wrap(() => num.parse(value));
+      });
 }
