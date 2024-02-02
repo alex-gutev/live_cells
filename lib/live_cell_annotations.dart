eb907ba047c5e8bdd575889105ace7cd1154853c
Alexander Gutev <alex.gutev@gmail.com>
2024-02-02 15:43:26 +0100

    Improve docstring for live_cell_annotations library.

--- lib/live_cell_annotations.dart
+++ lib/live_cell_annotations.dart
@@ -1,4 +1,4 @@
-/// Provides the annotations for the live_cell_extensions package.
+/// Provides annotations to use with the live_cell_extensions package.
 library live_cell_annotations;
 
 export 'src/annotations/annotations.dart' show CellExtension;
