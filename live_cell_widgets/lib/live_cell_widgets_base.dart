79122b37e3f0c2b114148f9bcd9ff4dc1e072202
Alexander Gutev <alex.gutev@gmail.com>
2024-02-08 20:55:25 +0100

    Add base exports to live_cell_widgets package.

---
+++ live_cell_widgets/lib/live_cell_widgets_base.dart
@@ -0,0 +1,15 @@
+/// Provides the base classes for integrating [ValueCell]'s with widgets.
+library live_cell_widgets_base;
+
+export 'src/extensions/widget_extension.dart'
+    show WidgetExtension, WidgetCellExtension, ComputeWidgetExtension;
+
+export 'src/cell_widget/cell_widget.dart'
+    show
+    CellWidget,
+    CreateCell,
+    CellInitializer,
+    CellWidgetContextExtension,
+    CellObserverState;
+
+export 'src/restoration/restorable_cell_widget.dart' show RestorableCellWidget;
