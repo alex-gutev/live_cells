f8b7a755741e2e369d4d0ba3215921f432b99921
Alexander Gutev <alex.gutev@gmail.com>
2024-02-08 20:56:04 +0100

    Change live_cells package exports to point to component packages.

--- live_cells/lib/live_cell_widgets.dart
+++ live_cells/lib/live_cell_widgets.dart
@@ -1,26 +1,3 @@
 library live_cell_widgets;
 
-export 'src/widgets/widgets.dart'
-    show
-        CellText,
-        CellCheckbox,
-        CellCheckboxListTile,
-        CellSlider,
-        CellRadio,
-        CellRadioListTile,
-        CellSwitch,
-        CellSwitchListTile;
-
-export 'src/widgets/cell_text_field.dart' show CellTextField;
-
-export 'src/widgets/extensions.dart'
-    show
-        WidgetCellValueExtension,
-        TextStyleCellValueExtension,
-        StrutStyleCellValueExtension,
-        TextAlignCellValueExtension,
-        LocaleCellValueExtension,
-        TextOverflowCellValueExtension,
-        TextScalerCellValueExtension,
-        TextWidthBasisCellValueExtension,
-        TextHeightBehaviorCellValueExtension;
+export 'package:live_cell_widgets/live_cell_widgets.dart';
--- live_cells/lib/live_cells.dart
+++ live_cells/lib/live_cells.dart
@@ -1,16 +1,4 @@
 library live_cells;
 
 export 'package:live_cells_core/live_cells_core.dart';
-
-export 'src/extensions/widget_extension.dart'
-    show WidgetExtension, WidgetCellExtension, ComputeWidgetExtension;
-
-export 'src/cell_widget/cell_widget.dart'
-    show
-        CellWidget,
-        CreateCell,
-        CellInitializer,
-        CellWidgetContextExtension,
-        CellObserverState;
-
-export 'src/restoration/restorable_cell_widget.dart' show RestorableCellWidget;
+export 'package:live_cell_widgets/live_cell_widgets_base.dart';
