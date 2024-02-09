d6415bb621a8a8ba83ea755dd3f653b15b2a9cef
Alexander Gutev <alex.gutev@gmail.com>
2024-02-09 08:39:08 +0100

    Make live_cell_widgets export all widget wrappers and extensions.

--- live_cell_widgets/lib/live_cell_widgets.dart
+++ live_cell_widgets/lib/live_cell_widgets.dart
@@ -2,31 +2,7 @@
 
 library live_cell_widgets;
 
-export 'src/widgets/widgets.dart'
-    show
-        CellText,
-        CellCheckbox,
-        CellCheckboxListTile,
-        CellSlider,
-        CellRadio,
-        CellRadioListTile,
-        CellSwitch,
-        CellSwitchListTile;
-
+export 'src/widgets/widgets.dart';
 export 'src/widgets/cell_text_field.dart' show CellTextField;
 
-export 'src/widgets/extensions.dart'
-    show
-        WidgetCellValueExtension,
-        TextStyleCellValueExtension,
-        StrutStyleCellValueExtension,
-        LocaleCellValueExtension,
-        TextScalerCellValueExtension,
-        TextHeightBehaviorCellValueExtension,
-        ColorCellValueExtension,
-        InputDecorationCellValueExtension,
-        TextAlignVerticalCellValueExtension,
-        RadiusCellValueExtension,
-        EdgeInsetsCellValueExtension,
-        TextSelectionControlsCellValueExtension,
-        TextInputTypeCellValueExtension;
+export 'src/widgets/extensions.dart';
