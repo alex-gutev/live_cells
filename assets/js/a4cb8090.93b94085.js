0f011ddf5564057532be3b34869562ea407cc070
Alexander Gutev <alex.gutev@gmail.com>
2024-02-18 19:34:15 +0100

    Update docs.

--- assets/js/a4cb8090.93b94085.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website_2=self.webpackChunkmy_website_2||[]).push([[240],{1966:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- assets/js/c369348f.eb970da3.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website_2=self.webpackChunkmy_website_2||[]).push([[153],{4061:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
