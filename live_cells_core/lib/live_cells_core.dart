191fda69312f0b2a55b5964e6a2b666f9a38bea0
Alexander Gutev <alex.gutev@gmail.com>
2024-03-02 18:05:53 +0100

    Simplify live_cells_core exports.

--- live_cells_core/lib/live_cells_core.dart
+++ live_cells_core/lib/live_cells_core.dart
@@ -23,67 +23,33 @@ export 'src/mutable_cell/mutable_cell_view.dart' show MutableCellView;
 
 export 'src/compute_cell/compute_cell.dart' show ComputeCell;
 export 'src/compute_cell/store_cell.dart' show StoreCell, StoreCellExtension;
-
-export 'src/extensions/compute_extension.dart'
-    show
-        ComputeExtension,
-        ListComputeExtension,
-        RecordComputeExtension2,
-        RecordComputeExtension3,
-        RecordComputeExtension4,
-        RecordComputeExtension5,
-        RecordComputeExtension6,
-        RecordComputeExtension7,
-        RecordComputeExtension8,
-        RecordComputeExtension9,
-        RecordComputeExtension10;
-
 export 'src/compute_cell/mutable_compute_cell.dart' show MutableComputeCell;
 export 'src/compute_cell/peek_cell.dart' show PeekCellExtension;
+
 export 'src/maybe_cell/maybe.dart'
     show Maybe, MaybeCell, CellMaybeExtension, MaybeCellExtension;
 
 export 'src/previous_values/prev_value_cell.dart'
     show PrevValueCell, PrevValueCellExtension;
 
-export 'src/extensions/numeric_extension.dart' show NumericExtension;
-
-export 'src/extensions/duration_extension.dart'
-    show
-        DurationCellExtension,
-        MutableDurationCellExtension;
-
-export 'src/extensions/bool_extension.dart' show BoolCellExtension;
-export 'src/extensions/error_handling_extension.dart' show ErrorCellExtension;
-
-export 'src/extensions/conversion_extensions.dart'
-    show
-        ValueCellExtension,
-        ParseIntExtension,
-        ParseDoubleExtension,
-        ParseNumExtension,
-        ParseMaybeIntExtension,
-        ParseMaybeDoubleExtension,
-        ParseMaybeNumExtension,
-        ConvertStringExtension;
-
-export 'src/extensions/list_cell_extension.dart'
-    show ListCellExtension, MutableListCellExtension;
-
-export 'src/extensions/iterable_cell_extension.dart' show IterableCellExtension;
-export 'src/extensions/set_cell_extension.dart' show SetCellExtension;
-
-export 'src/extensions/map_cell_extension.dart'
-    show MapCellExtension, MutableMapCellExtension;
-
-export 'src/extensions/wait_cell_extension.dart';
-export 'src/extensions/value_change_extension.dart';
-
 export 'src/cell_watch/cell_watcher.dart' show CellWatcher;
 
 export 'src/restoration/restoration.dart' show RestorableCell, CellValueCoder;
 
 export 'src/utilities/cell_observer_model.dart' show CellObserverModel;
+
+export 'src/extensions/compute_extension.dart';
+export 'src/extensions/numeric_extension.dart';
+export 'src/extensions/duration_extension.dart';
+export 'src/extensions/bool_extension.dart';
+export 'src/extensions/error_handling_extension.dart';
+export 'src/extensions/conversion_extensions.dart';
+export 'src/extensions/list_cell_extension.dart';
+export 'src/extensions/iterable_cell_extension.dart';
+export 'src/extensions/set_cell_extension.dart';
+export 'src/extensions/map_cell_extension.dart';
+export 'src/extensions/wait_cell_extension.dart';
+export 'src/extensions/value_change_extension.dart';
 
 export 'package:live_cell_annotations/live_cell_annotations.dart'
     show CellExtension;
