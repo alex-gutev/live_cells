d052557cdaf49066169fea7a26e59e634468efc5
Alexander Gutev <alex.gutev@gmail.com>
2024-03-08 12:18:14 +0100

    Add PersistentStatefulCell to live_cell_internals exports.

--- live_cells_core/lib/live_cells_internals.dart
+++ live_cells_core/lib/live_cells_internals.dart
@@ -6,7 +6,9 @@ library live_cells_internals;
 
 export 'src/value_cell.dart' show DependentCell;
 
-export 'src/compute_cell/dynamic_compute_cell.dart' show ComputeArgumentsTracker;
+export 'src/compute_cell/dynamic_compute_cell.dart'
+    show ComputeArgumentsTracker;
+
 export 'src/base/auto_key.dart' show AutoKey;
 export 'src/base/keys.dart';
 
@@ -17,6 +19,11 @@ export 'src/base/cell_observer.dart' show CellObserver;
 
 export 'src/stateful_cell/cell_state.dart' show CellState;
 export 'src/stateful_cell/observer_cell_state.dart' show ObserverCellState;
-export 'src/stateful_cell/stateful_cell.dart' show StatefulCell;
+
+export 'src/stateful_cell/stateful_cell.dart'
+    show
+        StatefulCell,
+        PersistentStatefulCell,
+        InactivePersistentStatefulCellError;
 
 export 'src/mutable_cell/mutable_dependent_cell.dart' show MutableDependentCell;
