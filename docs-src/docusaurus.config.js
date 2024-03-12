022cfd52d9794aa518fa69c7b5d6edb04f147362
Alexander Gutev <alex.gutev@gmail.com>
2024-03-12 22:11:37 +0100

    Update root domain of documentation site.

--- docs-src/docusaurus.config.js
+++ docs-src/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   // favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://alex-gutev.github.io',
+  url: 'https://livecells.viditrack.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/live_cells/',
