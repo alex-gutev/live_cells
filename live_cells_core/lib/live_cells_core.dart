c8ff0c8fcdc20dbd841f96b3206a84ba17799c4e
Alexander Gutev <alex.gutev@gmail.com>
2024-04-14 09:07:19 +0200

    Add StopComputeException to live_cells_core exports.

--- live_cells_core/lib/live_cells_core.dart
+++ live_cells_core/lib/live_cells_core.dart
@@ -5,8 +5,14 @@ library live_cells;
 export 'src/value_cell.dart' show ValueCell, ConstantCell;
 
 export 'src/base/types.dart' show WatchCallback;
+
 export 'src/base/exceptions.dart'
-    show UninitializedCellError, PendingAsyncValueError, NullCellError;
+    show
+        StopComputeException,
+        UninitializedCellError,
+        PendingAsyncValueError,
+        NullCellError;
+
 export 'src/base/none_cell.dart' show NoneCell;
 
 export 'src/mutable_cell/mutable_cell.dart' show MutableCell;
