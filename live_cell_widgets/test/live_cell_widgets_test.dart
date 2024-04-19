4bd315b67885c23296071d0a0312b2fd29f4fde0
Alexander Gutev <alex.gutev@gmail.com>
2024-04-19 08:55:06 +0200

    Add animation import to widget library unit tests.

--- live_cell_widgets/test/live_cell_widgets_test.dart
+++ live_cell_widgets/test/live_cell_widgets_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:live_cell_widgets/live_cell_widgets.dart';
