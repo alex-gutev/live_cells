e88f00f973fd07da33f6b6b8fb6196b6c402f47a
Alexander Gutev <alex.gutev@gmail.com>
2024-11-03 10:22:11 +0100

    Add deprecation notice to all widgets.

--- live_cell_widgets/lib/src/widgets/widgets.dart
+++ live_cell_widgets/lib/src/widgets/widgets.dart
@@ -3,7 +3,6 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:live_cell_widgets/src/extensions/widget_extension.dart';
 import 'package:live_cells_core/live_cells_core.dart';
 import 'package:live_cell_annotations/live_cell_annotations.dart';
@@ -12,70 +11,87 @@ import 'package:live_cells_core/live_cells_internals.dart';
 import '../cell_widget/cell_widget.dart';
 
 @GenerateCellWidgets([
-  WidgetSpec<AbsorbPointer>(),
+  WidgetSpec<AbsorbPointer>(
+    deprecationNotice: 'Use widgets from live_cells_ui library.'
+  ),
 
   WidgetSpec<Align>(
     includeSuperProperties: [#child],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<AnimatedAlign>(
       includeSuperProperties: [#curve, #duration, #onEnd],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<AnimatedContainer>(
       includeSuperProperties: [#curve, #duration, #onEnd],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<AnimatedCrossFade>(
       propertyDefaultValues: {
         #layoutBuilder: 'AnimatedCrossFade.defaultLayoutBuilder'
       },
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<AnimatedDefaultTextStyle>(
       includeSuperProperties: [#curve, #duration, #onEnd],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<AnimatedFractionallySizedBox>(
       includeSuperProperties: [#curve, #duration, #onEnd],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<AnimatedOpacity>(
       includeSuperProperties: [#curve, #duration, #onEnd],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<AnimatedPhysicalModel>(
       includeSuperProperties: [#curve, #duration, #onEnd],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<AnimatedPositioned>(
       includeSuperProperties: [#curve, #duration, #onEnd],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<AnimatedPositionedDirectional>(
       includeSuperProperties: [#curve, #duration, #onEnd],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<AnimatedRotation>(
       includeSuperProperties: [#curve, #duration, #onEnd],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<AnimatedScale>(
       includeSuperProperties: [#curve, #duration, #onEnd],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<AnimatedSize>(
       includeSuperProperties: [#curve, #duration, #onEnd],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<AnimatedSlide>(
       includeSuperProperties: [#curve, #duration, #onEnd],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<AnimatedSwitcher>(
       propertyTypes: {
         #layoutBuilder: 'AnimatedSwitcherLayoutBuilder'
       },
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<AnnotatedRegion>(
@@ -85,17 +101,22 @@ import '../cell_widget/cell_widget.dart';
       propertyTypes: {
         #value: 'T'
       },
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<AspectRatio>(
     includeSuperProperties: [#child],
+    deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<BackdropFilter>(
     includeSuperProperties: [#child],
+    deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
-  WidgetSpec<Badge>(),
+  WidgetSpec<Badge>(
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
+  ),
   WidgetSpec<Banner>(
     propertyDefaultValues: {
       #color: 'Color(0xA0B71C1C)',
@@ -104,22 +125,27 @@ import '../cell_widget/cell_widget.dart';
         'fontSize: 12.0 * 0.85,'
         'fontWeight: FontWeight.w900,'
         'height: 1.0)'
-    }
+    },
+    deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<Baseline>(
     includeSuperProperties: [#child],
+    deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<BlockSemantics>(
-    includeSuperProperties: [#child]
+    includeSuperProperties: [#child],
+    deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<Card>(
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<Center>(
     includeSuperProperties: [#widthFactor, #heightFactor, #child],
+    deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<Checkbox>(
@@ -145,7 +171,9 @@ The [value] is controlled by a [MutableCell] which is passed on construction.
 When the value of the cell changes, the checkbox state is updated to reflect the
 value of the cell. Similarly when the state of the checkbox is changed by the
 user, the value of the cell is updated to reflect the state.
-'''
+''',
+
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<CheckboxListTile>(
@@ -159,7 +187,9 @@ user, the value of the cell is updated to reflect the state.
       documentation: '''A [ListTile] with a [CellCheckbox], akin to [CheckboxListTile].
 
 See [CellCheckbox] for a more detailed explanation.
-'''
+''',
+
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<CheckboxMenuButton>(
@@ -201,11 +231,14 @@ See [CellCheckbox] for a more detailed explanation.
 
       documentation: '''A menu item with a [CellCheckbox], akin to [CheckboxMenuButton].
   
-  See [CellCheckbox] for a more detailed explanation.'''
+  See [CellCheckbox] for a more detailed explanation.''',
+
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<CheckboxTheme>(
-    includeSuperProperties: [#child]
+    includeSuperProperties: [#child],
+    deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<CheckedPopupMenuItem>(
@@ -243,15 +276,20 @@ See [CellCheckbox] for a more detailed explanation.
         meta: true,
         documentation: 'MetaCell for an action cell that is triggered when the '
             'item is tapped.'
-      )
-    ]
+      ),
+    ],
+
+    deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   // TODO: Fix this
-  WidgetSpec<Chip>(),
+  WidgetSpec<Chip>(
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
+  ),
 
   WidgetSpec<ChipTheme>(
-    includeSuperProperties: [#child]
+    includeSuperProperties: [#child],
+    deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<ChoiceChip>(
@@ -268,7 +306,8 @@ See [CellCheckbox] for a more detailed explanation.
 
             documentation: 'Is the widget enabled for user input?'
         )
-      ]
+      ],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<CircularProgressIndicator>(
@@ -284,18 +323,22 @@ See [CellCheckbox] for a more detailed explanation.
       propertyDefaultValues: {
         #strokeAlign: '0.0'
       },
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<ClipOval>(
       includeSuperProperties: [#child],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<ClipPath>(
       includeSuperProperties: [#child],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<ClipRect>(
       includeSuperProperties: [#child],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<CloseButton>(
@@ -329,6 +372,8 @@ See [CellCheckbox] for a more detailed explanation.
     baseClass: #_WrapperInterface,
     buildMethod: #_buildWrappedWidget,
 
+    deprecationNotice: 'Use widgets from live_cells_ui library.',
+
     documentation: '''A [CloseButton] widget with its properties controlled by [ValueCell]'s.
 
 The constructor takes mostly same arguments as the unnamed constructor of [CloseButton],
@@ -344,11 +389,13 @@ triggered when the button is pressed.
   ),
 
   WidgetSpec<ColoredBox>(
-    includeSuperProperties: [#child]
+    includeSuperProperties: [#child],
+    deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<ColorFiltered>(
-    includeSuperProperties: [#child]
+    includeSuperProperties: [#child],
+    deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<Column>(
@@ -365,22 +412,28 @@ triggered when the button is pressed.
       propertyValues: {
         #children: 'children.cellList().map((e) => e.widget()).toList()'
       },
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<ConstrainedBox>(
       includeSuperProperties: [#child],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<ConstraintsTransformBox>(
-    includeSuperProperties: [#child]
+    includeSuperProperties: [#child],
+    deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
-  WidgetSpec<Container>(),
+  WidgetSpec<Container>(
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
+  ),
 
   WidgetSpec<CupertinoActivityIndicator>(
       propertyDefaultValues: {
         #radius: '10.0'
       },
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<CupertinoListSection>(
@@ -389,11 +442,16 @@ triggered when the button is pressed.
         #dividerMargin: '20.0',
         #topMargin: '22.0',
       },
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
   
-  WidgetSpec<CupertinoPageScaffold>(),
+  WidgetSpec<CupertinoPageScaffold>(
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
+  ),
 
-  WidgetSpec<CupertinoPopupSurface>(),
+  WidgetSpec<CupertinoPopupSurface>(
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
+  ),
 
   WidgetSpec<CupertinoSlider>(
       mutableProperties: [#value],
@@ -418,7 +476,9 @@ triggered when the button is pressed.
 The [value] is controlled by a [MutableCell] which is passed on construction. 
 When the value of the cell changes, the slider position is updated to reflect 
 the value of the cell. Similarly when the slider is moved by the user, the 
-value of the cell is updated to reflect the slider position.'''
+value of the cell is updated to reflect the slider position.''',
+
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<CupertinoSwitch>(
@@ -443,19 +503,24 @@ The [value] is controlled by a [MutableCell] which is passed on construction.
 When the value of the cell changes, the switch state is updated to reflect the
 value of the cell. Similarly when the state of the switch is changed by the
 user, the value of the cell is updated to reflect the state.
-'''
+''',
 
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<DecoratedBox>(
       includeSuperProperties: [#child],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<DefaultTextStyle>(
       includeSuperProperties: [#child],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
-  WidgetSpec<Divider>(),
+  WidgetSpec<Divider>(
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
+  ),
 
   WidgetSpec<ElevatedButton>(
       includeSuperProperties: [
@@ -525,11 +590,14 @@ Instead of taking callback functions for [onPressed] and [onLongPress], the
 constructor accepts [MetaCell]'s that are set to [ActionCell]s which are
 triggered when the button is pressed and long pressed respectively. Similarly,
 [bool] [MetaCell]s are accepted for [onHover] and [onFocusChange].
-'''
+''',
+
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<ExcludeSemantics>(
       includeSuperProperties: [#child],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<FilledButton>(
@@ -600,37 +668,48 @@ Instead of taking callback functions for [onPressed] and [onLongPress], the
 constructor accepts [MetaCell]'s that are set to [ActionCell]s which are
 triggered when the button is pressed and long pressed respectively. Similarly,
 [bool] [MetaCell]s are accepted for [onHover] and [onFocusChange].
-'''
+''',
+
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<FittedBox>(
       includeSuperProperties: [#child],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<FractionalTranslation>(
       includeSuperProperties: [#child],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<FractionallySizedBox>(
       includeSuperProperties: [#child],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
-  WidgetSpec<Icon>(),
+  WidgetSpec<Icon>(
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
+  ),
 
   WidgetSpec<IgnorePointer>(
       includeSuperProperties: [#child],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<IntrinsicHeight>(
       includeSuperProperties: [#child],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<IntrinsicWidth>(
       includeSuperProperties: [#child],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<LimitedBox>(
       includeSuperProperties: [#child],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<LinearProgressIndicator>(
@@ -641,12 +720,16 @@ triggered when the button is pressed and long pressed respectively. Similarly,
         #semanticsLabel,
         #semanticsValue
       ],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
-  WidgetSpec<ListTile>(),
+  WidgetSpec<ListTile>(
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
+  ),
 
   WidgetSpec<Text>(
     as: #CellText,
+    deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<PageView>(
@@ -741,7 +824,9 @@ the value of the [page] cell or trigger [nextPage]/[previousPage].'''
       #onPageChanged: '''(index) => _withSuppressed(() {
         widget.page.value = index;
       })'''
-    }
+    },
+
+    deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<Slider>(
@@ -760,6 +845,7 @@ the value of the [page] cell or trigger [nextPage]/[previousPage].'''
             documentation: 'Is the widget enabled for user input?'
         )
       ],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<Switch>(
@@ -778,6 +864,7 @@ the value of the [page] cell or trigger [nextPage]/[previousPage].'''
             documentation: 'Is the widget enabled for user input?'
         )
       ],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<SwitchListTile>(
@@ -797,6 +884,7 @@ the value of the [page] cell or trigger [nextPage]/[previousPage].'''
             documentation: 'Is the widget enabled for user input?'
         )
       ],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<Radio>(
@@ -822,6 +910,7 @@ the value of the [page] cell or trigger [nextPage]/[previousPage].'''
             documentation: 'Is the widget enabled for user input?'
         )
       ],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<RadioListTile>(
@@ -847,6 +936,7 @@ the value of the [page] cell or trigger [nextPage]/[previousPage].'''
             documentation: 'Is the widget enabled for user input?'
         )
       ],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<Row>(
@@ -863,6 +953,7 @@ the value of the [page] cell or trigger [nextPage]/[previousPage].'''
       propertyValues: {
         #children: 'children.cellList().map((e) => e.widget()).toList()'
       },
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<OutlinedButton>(
@@ -933,7 +1024,9 @@ Instead of taking callback functions for [onPressed] and [onLongPress], the
 constructor accepts [MetaCell]'s that are set to [ActionCell]s which are
 triggered when the button is pressed and long pressed respectively. Similarly,
 [bool] [MetaCell]s are accepted for [onHover] and [onFocusChange].
-'''
+''',
+
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<TextButton>(
@@ -1004,11 +1097,14 @@ Instead of taking callback functions for [onPressed] and [onLongPress], the
 constructor accepts [MetaCell]'s that are set to [ActionCell]s which are
 triggered when the button is pressed and long pressed respectively. Similarly,
 [bool] [MetaCell]s are accepted for [onHover] and [onFocusChange].
-'''
+''',
+
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 
   WidgetSpec<Visibility>(
       includeSuperProperties: [#child],
+      deprecationNotice: 'Use widgets from live_cells_ui library.'
   ),
 ])
 part 'widgets.g.dart';
