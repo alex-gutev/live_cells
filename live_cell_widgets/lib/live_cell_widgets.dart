f5a43c3fd91325979b6e333e791ad641fd1873e0
Alexander Gutev <alex.gutev@gmail.com>
2024-11-03 11:01:47 +0100

    Add live_cells_ui library.

--- live_cell_widgets/lib/live_cell_widgets.dart
+++ live_cell_widgets/lib/live_cell_widgets.dart
@@ -1,5 +1,6 @@
 /// Provides wrappers over Flutter widgets which allow their properties to be controlled by [ValueCell]'s.
 
+@Deprecated('User live_cells_ui library instead.')
 library live_cell_widgets;
 
 export 'src/widgets/widgets.dart';
