3a04ea02b67726af1eabbeced55c60a2c0e8b788
liquidiert <haberederkorbinian@googlemail.com>
2024-12-15 10:12:44 +0100

    feat: number format for mutableString - add the ability to assign a NumberFormat object - add unit tests for german formats

--- live_cells_core/lib/src/extensions/conversion_extensions.dart
+++ live_cells_core/lib/src/extensions/conversion_extensions.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+
 import '../base/exceptions.dart';
 import '../base/keys.dart';
 import '../compute_cell/compute_cell.dart';
@@ -16,9 +18,7 @@ extension ValueCellExtension<T> on T {
   /// Create a [MutableCell] with value initialized to [this].
   ///
   /// **Note**: the cell is created with the "reset" parameter set to true.
-  MutableCell<T> get mutableCell => MutableCell(this,
-    reset: true
-  );
+  MutableCell<T> get mutableCell => MutableCell(this, reset: true);
 }
 
 /// Provides methods for converting an integer to a string and vice versa.
@@ -32,24 +32,30 @@ extension ParseIntExtension on MutableCell<int> {
   /// [errorValue] is the value to assign to [this] cell when there is an
   /// error parsing an integer from the string.
   ///
+  /// [format] is of type [NumberFormat] and is used to format the string
+  /// representation of the [num] value.
+  ///
   /// **Note** [errorValue] is a [ValueCell]'s to allow the default value to be
   /// chosen dynamically. If a constant is required use the
   /// [NumValueCellExtension.cell] property e.g. `10.cell`.
   ///
   /// **Pitfalls**:
-  /// 
+  ///
   /// A binding is not established between [errorValue] and [this] cell. This
   /// means that [this] cell will not react to changes in [errorValue]. Only
   /// the values at the time of assigning a value to the returned cell are used.
   MutableCell<String> mutableString({
     ValueCell<int> errorValue = const ValueCell.value(0),
-  }) => MutableComputeCell(
-      arguments: {this},
-      compute: () => value.toString(),
-      reverseCompute: (value) {
-        this.value = int.tryParse(value) ?? errorValue.value;
-      }
-  );
+    NumberFormat? format,
+  }) =>
+      MutableComputeCell(
+          arguments: {this},
+          compute: () => format?.format(value) ?? value.toString(),
+          reverseCompute: (value) {
+            this.value = format?.tryParse(value)?.toInt() ??
+                int.tryParse(value) ??
+                errorValue.value;
+          });
 }
 
 /// Provides methods for converting a double to a string and vice versa.
@@ -63,6 +69,9 @@ extension ParseDoubleExtension on MutableCell<double> {
   /// [errorValue] is the value to assign to [this] cell when there is an
   /// error parsing a a double from the string.
   ///
+  /// [format] is of type [NumberFormat] and is used to format the string
+  /// representation of the [num] value.
+  ///
   /// **Note** [errorValue] is a [ValueCell] to allow the default value
   /// to be chosen dynamically. If a constant is required use
   /// the [NumValueCellExtension.cell] property e.g. `1.0.cell`.
@@ -75,13 +84,17 @@ extension ParseDoubleExtension on MutableCell<double> {
   /// cell are used.
   MutableCell<String> mutableString({
     ValueCell<double> errorValue = const ValueCell.value(0.0),
-  }) => MutableComputeCell(
-      arguments: {this},
-      compute: () => value.toString(),
-      reverseCompute: (value) {
-        this.value = double.tryParse(value) ?? errorValue.value;
-      },
-  );
+    NumberFormat? format,
+  }) =>
+      MutableComputeCell(
+        arguments: {this},
+        compute: () => format?.format(value) ?? value.toString(),
+        reverseCompute: (value) {
+          this.value = format?.tryParse(value)?.toDouble() ??
+              double.tryParse(value) ??
+              errorValue.value;
+        },
+      );
 }
 
 /// Provides methods for converting a num to a string and vice versa.
@@ -95,6 +108,9 @@ extension ParseNumExtension on MutableCell<num> {
   /// [errorValue] is the value to assign to [this] cell when there is an
   /// error parsing a [num] from the string.
   ///
+  /// [format] is of type [NumberFormat] and is used to format the string
+  /// representation of the [num] value.
+  ///
   /// **Note** [errorValue] is a [ValueCell] to allow the default value
   /// to be chosen dynamically. If a constant is required use
   /// the [NumValueCellExtension.cell] property e.g. `1.0.cell`.
@@ -107,13 +123,16 @@ extension ParseNumExtension on MutableCell<num> {
   /// cell are used.
   MutableCell<String> mutableString({
     ValueCell<num> errorValue = const ValueCell.value(0),
-  }) => MutableComputeCell(
-      arguments: {this},
-      compute: () => value.toString(),
-      reverseCompute: (value) {
-        this.value = num.tryParse(value) ?? errorValue.value;
-      }
-  );
+    NumberFormat? format,
+  }) =>
+      MutableComputeCell(
+          arguments: {this},
+          compute: () => format?.format(value) ?? value.toString(),
+          reverseCompute: (value) {
+            this.value = format?.tryParse(value) ??
+                num.tryParse(value) ??
+                errorValue.value;
+          });
 }
 
 /// Provides the [mutableString] method for String cells
@@ -132,14 +151,15 @@ extension ParseMaybeIntExtension on MaybeCell<int> {
   ///
   /// If an exception is thrown during parsing, a [Maybe] holding the exception
   /// is assigned to this cell's value.
-  MutableCell<String> mutableString() =>
+  MutableCell<String> mutableString({NumberFormat? format}) =>
       MutableComputeCell(
           arguments: {this},
-          compute: () => value.unwrap.toString(),
+          compute: () =>
+              format?.format(value.unwrap) ?? value.unwrap.toString(),
           reverseCompute: (value) {
-            this.value = Maybe.wrap(() => int.parse(value));
-          }
-      );
+            this.value = Maybe.wrap(
+                () => format?.tryParse(value)?.toInt() ?? int.parse(value));
+          });
 }
 
 /// Provides methods for converting a double to a string and vice versa with error handling.
@@ -152,14 +172,15 @@ extension ParseMaybeDoubleExtension on MaybeCell<double> {
   ///
   /// If an exception is thrown during parsing, a [Maybe] holding the exception
   /// is assigned to this cell's value.
-  MutableCell<String> mutableString() =>
+  MutableCell<String> mutableString({NumberFormat? format}) =>
       MutableComputeCell(
           arguments: {this},
-          compute: () => value.unwrap.toString(),
+          compute: () =>
+              format?.format(value.unwrap) ?? value.unwrap.toString(),
           reverseCompute: (value) {
-            this.value = Maybe.wrap(() => double.parse(value));
-          }
-      );
+            this.value = Maybe.wrap(() =>
+                format?.tryParse(value)?.toDouble() ?? double.parse(value));
+          });
 }
 
 /// Provides methods for converting a [num] to a string and vice versa with error handling.
@@ -172,14 +193,15 @@ extension ParseMaybeNumExtension on MaybeCell<num> {
   ///
   /// If an exception is thrown during parsing, a [Maybe] holding the exception
   /// is assigned to this cell's value.
-  MutableCell<String> mutableString() =>
+  MutableCell<String> mutableString({NumberFormat? format}) =>
       MutableComputeCell(
           arguments: {this},
-          compute: () => value.unwrap.toString(),
+          compute: () =>
+              format?.format(value.unwrap) ?? value.unwrap.toString(),
           reverseCompute: (value) {
-            this.value = Maybe.wrap(() => num.parse(value));
-          }
-      );
+            this.value =
+                Maybe.wrap(() => format?.tryParse(value) ?? num.parse(value));
+          });
 }
 
 /// Provides methods that check for and handle null values.
@@ -189,8 +211,7 @@ extension NullCheckExtension<T> on ValueCell<T?> {
   /// If the value of [this] cell is null, accessing the value of the [notNull]
   /// cell results in a [NullCellError] exception being thrown.
   ValueCell<T> get notNull => apply((value) => value ?? (throw NullCellError()),
-    key: _NullCheckExtensionKey(this)
-  );
+      key: _NullCheckExtensionKey(this));
 
   /// Replace null values in this cell with value of another cell.
   ///
@@ -198,25 +219,24 @@ extension NullCheckExtension<T> on ValueCell<T?> {
   /// If the value of this cell is null, the value of the returned cell is the
   /// value of the [ifNull] cell.
   ValueCell<T> coalesce(ValueCell<T> ifNull) => ComputeCell(
-    arguments: {this, ifNull},
-    key: _NullCheckCoalesceKey(this, ifNull),
-    compute: () => value ?? ifNull.value
-  ).store(changesOnly: true);
+      arguments: {this, ifNull},
+      key: _NullCheckCoalesceKey(this, ifNull),
+      compute: () => value ?? ifNull.value).store(changesOnly: true);
 }
 
 /// Provides methods that check for null values.
 extension MutableNullCheckExtension<T> on MutableCell<T?> {
   /// MutableCell version of [NullCheckExtension.notNull].
-  MutableCell<T> get notNull => (this as ValueCell<T?>).notNull.mutableApply((p0) => p0, (p0) {
-    value = p0;
-  }, key: _MutableNullCheckExtensionKey(this));
+  MutableCell<T> get notNull =>
+      (this as ValueCell<T?>).notNull.mutableApply((p0) => p0, (p0) {
+        value = p0;
+      }, key: _MutableNullCheckExtensionKey(this));
 
   /// Mutable version of [NullCheckExtension.coalesce].
   ///
   /// Setting the value of the returned cell sets the value of [this] cell.
-  MutableCell<T> coalesce(ValueCell<T> ifNull) => (this as ValueCell<T?>)
-      .coalesce(ifNull)
-      .mutableApply((p0) => p0, (p0) {
+  MutableCell<T> coalesce(ValueCell<T> ifNull) =>
+      (this as ValueCell<T?>).coalesce(ifNull).mutableApply((p0) => p0, (p0) {
         value = p0;
       }, key: _MutableNullCheckCoalesceKey(this, ifNull));
 }
@@ -237,6 +257,7 @@ class _NullCheckCoalesceKey<T> extends CellKey2<ValueCell<T?>, ValueCell<T>> {
 }
 
 /// Key identifying a cell created with [MutableNullCheckExtension.coalesce].
-class _MutableNullCheckCoalesceKey<T> extends CellKey2<MutableCell<T?>, ValueCell<T>> {
+class _MutableNullCheckCoalesceKey<T>
+    extends CellKey2<MutableCell<T?>, ValueCell<T>> {
   _MutableNullCheckCoalesceKey(super.value1, super.value2);
 }
